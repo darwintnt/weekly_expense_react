c2e02db8a86baab5fbb0ebad9ce8e5680df584dd
Darwin Gomez <desarrollador@fhuryx.com>
2019-05-09 19:34:40 -0500

    Updates

---
+++ precache-manifest.2aef56dd7ffe1dd9881aa8d5c50f8804.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4648c5c437091d05c9d975b02041a5b8",
+    "url": "/weekly_expense_react/index.html"
+  },
+  {
+    "revision": "a6ae85f4266627b81382",
+    "url": "/weekly_expense_react/static/css/2.424bd964.chunk.css"
+  },
+  {
+    "revision": "406bf4f3952f86a9e21a",
+    "url": "/weekly_expense_react/static/css/main.ae4c1bed.chunk.css"
+  },
+  {
+    "revision": "a6ae85f4266627b81382",
+    "url": "/weekly_expense_react/static/js/2.dc155f86.chunk.js"
+  },
+  {
+    "revision": "406bf4f3952f86a9e21a",
+    "url": "/weekly_expense_react/static/js/main.a8275ade.chunk.js"
+  },
+  {
+    "revision": "8952a1b123acb59ec35a",
+    "url": "/weekly_expense_react/static/js/runtime~main.4decaa87.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/repository-name/precache-manifest.3a1a9bcd6a67846ee55478c343d2b711.js"
+  "/weekly_expense_react/precache-manifest.2aef56dd7ffe1dd9881aa8d5c50f8804.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/repository-name/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weekly_expense_react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
